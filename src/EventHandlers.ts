70df0045726fc26253e8273c3abbf5f63883d3a5
moose-code <jonjonclark@gmail.com>
2025-07-29 16:09:15 +0100

    liquidations init

---
+++ src/EventHandlers.ts
@@ -0,0 +1,22 @@
+/*
+ * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
+ */
+import {
+  InitializableImmutableAdminUpgradeabilityProxy,
+  InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall,
+} from "generated";
+
+InitializableImmutableAdminUpgradeabilityProxy.LiquidationCall.handler(async ({ event, context }) => {
+  const entity: InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    collateralAsset: event.params.collateralAsset,
+    debtAsset: event.params.debtAsset,
+    user: event.params.user,
+    debtToCover: event.params.debtToCover,
+    liquidatedCollateralAmount: event.params.liquidatedCollateralAmount,
+    liquidator: event.params.liquidator,
+    receiveAToken: event.params.receiveAToken,
+  };
+
+  context.InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall.set(entity);
+});
---
+++ test/Test.ts
@@ -0,0 +1,41 @@
+import assert from "assert";
+import { 
+  TestHelpers,
+  InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall
+} from "generated";
+const { MockDb, InitializableImmutableAdminUpgradeabilityProxy } = TestHelpers;
+
+describe("InitializableImmutableAdminUpgradeabilityProxy contract LiquidationCall event tests", () => {
+  // Create mock db
+  const mockDb = MockDb.createMockDb();
+
+  // Creating mock for InitializableImmutableAdminUpgradeabilityProxy contract LiquidationCall event
+  const event = InitializableImmutableAdminUpgradeabilityProxy.LiquidationCall.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+
+  it("InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall is created correctly", async () => {
+    // Processing the event
+    const mockDbUpdated = await InitializableImmutableAdminUpgradeabilityProxy.LiquidationCall.processEvent({
+      event,
+      mockDb,
+    });
+
+    // Getting the actual entity from the mock database
+    let actualInitializableImmutableAdminUpgradeabilityProxyLiquidationCall = mockDbUpdated.entities.InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall.get(
+      `${event.chainId}_${event.block.number}_${event.logIndex}`
+    );
+
+    // Creating the expected entity
+    const expectedInitializableImmutableAdminUpgradeabilityProxyLiquidationCall: InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall = {
+      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+      collateralAsset: event.params.collateralAsset,
+      debtAsset: event.params.debtAsset,
+      user: event.params.user,
+      debtToCover: event.params.debtToCover,
+      liquidatedCollateralAmount: event.params.liquidatedCollateralAmount,
+      liquidator: event.params.liquidator,
+      receiveAToken: event.params.receiveAToken,
+    };
+    // Asserting that the entity in the mock database is the same as the expected entity
+    assert.deepEqual(actualInitializableImmutableAdminUpgradeabilityProxyLiquidationCall, expectedInitializableImmutableAdminUpgradeabilityProxyLiquidationCall, "Actual InitializableImmutableAdminUpgradeabilityProxyLiquidationCall should be the same as the expectedInitializableImmutableAdminUpgradeabilityProxyLiquidationCall");
+  });
+});
