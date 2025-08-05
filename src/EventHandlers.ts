626e16db828944929d3b4f9a509c36930a83e872
moose-code <jonjonclark@gmail.com>
2025-08-05 18:36:31 +0100

    aave renamed

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,29 +1,21 @@
 /*
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
-import {
-  InitializableImmutableAdminUpgradeabilityProxy,
-  InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall,
-} from "generated";
+import { AaveProxy, AaveProxy_LiquidationCall } from "generated";
 
-InitializableImmutableAdminUpgradeabilityProxy.LiquidationCall.handler(
-  async ({ event, context }) => {
-    const entity: InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall =
-      {
-        id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-        chainId: event.chainId,
-        timestamp: BigInt(event.block.timestamp),
-        collateralAsset: event.params.collateralAsset,
-        debtAsset: event.params.debtAsset,
-        user: event.params.user,
-        debtToCover: event.params.debtToCover,
-        liquidatedCollateralAmount: event.params.liquidatedCollateralAmount,
-        liquidator: event.params.liquidator,
-        receiveAToken: event.params.receiveAToken,
-      };
+AaveProxy.LiquidationCall.handler(async ({ event, context }) => {
+  const entity: AaveProxy_LiquidationCall = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    chainId: event.chainId,
+    timestamp: BigInt(event.block.timestamp),
+    collateralAsset: event.params.collateralAsset,
+    debtAsset: event.params.debtAsset,
+    user: event.params.user,
+    debtToCover: event.params.debtToCover,
+    liquidatedCollateralAmount: event.params.liquidatedCollateralAmount,
+    liquidator: event.params.liquidator,
+    receiveAToken: event.params.receiveAToken,
+  };
 
-    context.InitializableImmutableAdminUpgradeabilityProxy_LiquidationCall.set(
-      entity
-    );
-  }
-);
+  context.AaveProxy_LiquidationCall.set(entity);
+});
